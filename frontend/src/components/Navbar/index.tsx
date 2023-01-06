b21439bafb8d438e554e34dacd79de723b67c00e
Guilherme <guilherme.beutler@outlook.com>
2023-01-06 17:32:05 -0300

    changes in navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import { Row, Button, Col, Input, Space } from 'antd'
+import {
+	SearchOutlined,
+	ShoppingOutlined,
+	UserOutlined,
+} from '@ant-design/icons'
 
-function Navbar() {
+function NavBar() {
 	return (
-		<div>
-			<p>THIS IS A NAVBAR</p>
-		</div>
+		<Row
+			style={{
+				backgroundColor: 'grey',
+				height: '50px',
+				alignItems: 'center',
+				justifyContent: 'space-around',
+				fontFamily: 'Cinzel',
+			}}
+		>
+			<h3 className="logo">Da MARGI</h3>
+			<Input
+				placeholder="Search"
+				style={{ width: 200 }}
+				prefix={<SearchOutlined />}
+			/>
+			<Col>
+				<Space>
+					<Button shape="circle" icon={<UserOutlined />} />
+					<Button shape="circle" icon={<ShoppingOutlined />} />
+				</Space>
+			</Col>
+		</Row>
 	)
 }
 
-export default Navbar
+export default NavBar
