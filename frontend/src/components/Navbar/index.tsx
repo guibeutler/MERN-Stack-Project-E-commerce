52aa38bae93a3c58f7c242d3ff2fdb8185c95b2c
Guilherme <guilherme.beutler@outlook.com>
2023-01-11 15:12:53 -0300

    create dropdown navigate

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
-import { Row, Col, Input, Space, Badge } from 'antd'
-import { ShoppingOutlined, UserOutlined } from '@ant-design/icons'
+import { useNavigate } from 'react-router-dom'
+import { Row, Col, Input, Space, Badge, Dropdown, Menu } from 'antd'
+import { DownOutlined, ShoppingOutlined, UserOutlined } from '@ant-design/icons'
 import './style.css'
-
 const { Search } = Input
 
 function NavBar() {
+	const navigate = useNavigate()
+
+	const handleNavigate = (key: string) => {
+		switch (key) {
+			case '1':
+				navigate('/user/my-orders')
+				break
+			case '2':
+				navigate('/user/profile')
+				break
+			case '3':
+				navigate('/logout')
+				break
+		}
+	}
+
 	return (
 		<div className="main-navbar">
 			<Row
@@ -27,10 +43,30 @@ function NavBar() {
 				/>
 				<Col>
 					<Space>
-						<UserOutlined className="icon" style={{ fontSize: '22px' }} />
+						<Badge dot>
+							<UserOutlined className="icon" style={{ fontSize: '22px' }} />
+						</Badge>
+
 						<Badge count={5} size={'small'}>
 							<ShoppingOutlined className="icon" style={{ fontSize: '22px' }} />
 						</Badge>
+						<Dropdown
+							trigger={['click']}
+							overlay={
+								<Menu onClick={({ key }) => handleNavigate(key)}>
+									<Menu.Item key="1">Meus pedidos</Menu.Item>
+									<Menu.Item key="2">Meu perfil</Menu.Item>
+									<Menu.Item key="3">Logout</Menu.Item>
+								</Menu>
+							}
+						>
+							<p onClick={(e) => e.preventDefault()}>
+								<Space>
+									Margi
+									<DownOutlined />
+								</Space>
+							</p>
+						</Dropdown>
 					</Space>
 				</Col>
 			</Row>
