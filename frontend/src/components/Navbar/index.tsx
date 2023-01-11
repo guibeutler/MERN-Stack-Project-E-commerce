a947498ce13d5f61d1cf01fd84adad76123791a6
Guilherme <guilherme.beutler@outlook.com>
2023-01-11 15:54:02 -0300

    inclusion login and register dropdown user icon

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Row, Col, Input, Space, Badge, Dropdown, Menu } from 'antd'
-import { DownOutlined, ShoppingOutlined, UserOutlined } from '@ant-design/icons'
+import {
+	ShoppingOutlined,
+	UserOutlined,
+	DownSquareOutlined,
+} from '@ant-design/icons'
 import './style.css'
+
 const { Search } = Input
 
 function NavBar() {
@@ -14,10 +19,16 @@ function NavBar() {
 				navigate('/user/my-orders')
 				break
 			case '2':
-				navigate('/user/profile')
+				navigate('/user')
 				break
 			case '3':
 				navigate('/logout')
+				break
+			case '4':
+				navigate('/')
+				break
+			case '5':
+				navigate('/')
 				break
 		}
 	}
@@ -35,7 +46,9 @@ function NavBar() {
 					fontFamily: 'Cinzel',
 				}}
 			>
-				<h2 className="logo">Da Margi</h2>
+				<h2 className="logo" onClick={() => navigate('/')}>
+					Da Margi
+				</h2>
 				<Search
 					placeholder="Pesquisar produto"
 					allowClear
@@ -43,10 +56,26 @@ function NavBar() {
 				/>
 				<Col>
 					<Space>
-						<Badge dot>
-							<UserOutlined className="icon" style={{ fontSize: '22px' }} />
-						</Badge>
-
+						<Dropdown
+							trigger={['click']}
+							overlay={
+								<Menu onClick={({ key }) => handleNavigate(key)}>
+									<Menu.Item key="4">Login</Menu.Item>
+									<Menu.Item key="5">Cadastrar</Menu.Item>
+								</Menu>
+							}
+						>
+							<p onClick={(e) => e.preventDefault()}>
+								<Space>
+									<Badge dot>
+										<UserOutlined
+											className="icon"
+											style={{ fontSize: '22px' }}
+										/>
+									</Badge>
+								</Space>
+							</p>
+						</Dropdown>
 						<Badge count={5} size={'small'}>
 							<ShoppingOutlined className="icon" style={{ fontSize: '22px' }} />
 						</Badge>
@@ -63,7 +92,7 @@ function NavBar() {
 							<p onClick={(e) => e.preventDefault()}>
 								<Space>
 									Margi
-									<DownOutlined />
+									<DownSquareOutlined />
 								</Space>
 							</p>
 						</Dropdown>
