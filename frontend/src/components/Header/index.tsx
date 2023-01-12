a5aa311e0e18bc1395984f2cfb39d465d73cc89c
Guilherme <guilherme.beutler@outlook.com>
2023-01-12 20:30:05 -0300

    adjustments burguer menu

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { Row, Col, Input, Space, Badge, Dropdown, Menu, Button } from 'antd'
+import { Badge } from 'antd'
 import {
 	ShoppingOutlined,
 	UserOutlined,
@@ -11,27 +10,6 @@ import './style.css'
 
 const Header = () => {
 	const [classOn, setClassOn] = useState(false)
-	const navigate = useNavigate()
-
-	const handleNavigate = (key: string) => {
-		switch (key) {
-			case '1':
-				navigate('/user/my-orders')
-				break
-			case '2':
-				navigate('/user')
-				break
-			case '3':
-				navigate('/logout')
-				break
-			case '4':
-				navigate('/')
-				break
-			case '5':
-				navigate('/')
-				break
-		}
-	}
 
 	return (
 		<header>
@@ -51,19 +29,19 @@ const Header = () => {
 					<nav>
 						<ul>
 							<li>
-								<a href="/">user</a>
+								<a href="/user">user</a>
 							</li>
 							<li>
 								<a href="/">login</a>
 							</li>
 							<li>
-								<a href="/">meus pedidos</a>
+								<a href="/user/my-orders">meus pedidos</a>
 							</li>
 							<li>
-								<a href="/">meu perfil</a>
+								<a href="/user">meu perfil</a>
 							</li>
 							<li>
-								<Badge count={5} size={'small'}>
+								<Badge className="badge" count={5} size={'small'}>
 									<ShoppingOutlined
 										className="icon"
 										style={{
