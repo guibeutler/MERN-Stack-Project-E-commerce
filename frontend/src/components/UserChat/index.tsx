2b85f6f6adb011972f95b0916ed73dc3957ad5ee
Guilherme <guilherme.beutler@outlook.com>
2023-01-20 16:51:38 -0300

    create btn chat

--- frontend/src/components/UserChat/index.tsx
+++ frontend/src/components/UserChat/index.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { CloseCircleOutlined, MessageOutlined } from '@ant-design/icons'
+import './style.css'
 
 function UserChat() {
+	const [isChecked, setIsChecked] = useState(false)
+
 	return (
 		<div>
-			<p>CHAT</p>
+			<input
+				type="checkbox"
+				id="check"
+				checked={isChecked}
+				onChange={(e) => setIsChecked(e.target.checked)}
+			/>
+			<label className="chat-btn" htmlFor="check">
+				<CloseCircleOutlined
+					className="close"
+					style={{ display: isChecked ? 'block' : 'none' }}
+				/>
+				<MessageOutlined
+					className="comment"
+					style={{ display: isChecked ? 'none' : 'block' }}
+				/>
+			</label>
 		</div>
 	)
 }
