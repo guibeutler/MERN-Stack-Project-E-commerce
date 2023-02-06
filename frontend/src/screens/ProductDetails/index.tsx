38f86b6218d5c3e244d79896214b6a3131076638
Guilherme <guilherme.beutler@outlook.com>
2023-02-06 14:10:17 -0300

    include layout screen productDetails

--- frontend/src/screens/ProductDetails/index.tsx
+++ frontend/src/screens/ProductDetails/index.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import { Col, Layout, Row } from 'antd'
 
 function ProductDetails() {
 	return (
-		<div>
-			<p>PRODUCT DETAILS</p>
-		</div>
+		<Layout style={{ backgroundColor: '#fff' }}>
+			<Row>
+				<Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}>
+					Images
+				</Col>
+				<Col xs={{ span: 11, offset: 1 }} lg={{ span: 6, offset: 2 }}>
+					<Row>
+						<Col md={8}> Product name, price, description, rating</Col>
+					</Row>
+					<Row>
+						<Col>
+							<h5>REVIEWS</h5>
+						</Col>
+					</Row>
+					<hr />
+					send review form
+				</Col>
+				<Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}>
+					<p>Product status, quantity, add to cart</p>
+				</Col>
+			</Row>
+		</Layout>
 	)
 }
 
