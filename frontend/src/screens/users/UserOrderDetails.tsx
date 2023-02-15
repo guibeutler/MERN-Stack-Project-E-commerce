17d9733b0c0e57dffa1d5e9dbd196ab2553959f6
Guilherme <guilherme.beutler@outlook.com>
2023-02-15 13:43:08 -0300

    adjustment order details screen

--- frontend/src/screens/users/UserOrderDetails.tsx
+++ frontend/src/screens/users/UserOrderDetails.tsx
@@ -1,27 +1,77 @@
 import React from 'react'
-import { Col, Layout, Row } from 'antd'
+import { Alert, Col, List, Row, Select } from 'antd'
+import CartItemComponent from '@components/CartItemComponent'
+
+const shipping = [
+	'Nome: Jonh Doe',
+	'Endereço: São Teodoro, 577',
+	'Telefone: 55 55555-5555',
+	<Alert message="Error Text" type="error" />,
+]
+
+const paymentMethod = [
+	<Select
+		style={{ width: 250 }}
+		options={[{ value: 'Paypal', label: 'Paypal' }]}
+	/>,
+	<Alert message="Success Text" type="success" />,
+]
+
+const orderSummary = [
+	'Valor: R$ 55.00',
+	'Envio: incluso',
+	'Total: R$ 55.00',
+	<Alert message="Error Text" type="error" />,
+]
 
 function UserOrderDetails() {
 	return (
-		<div
-		// style={{
-		// 	display: 'flex',
-		// 	flexDirection: 'column',
-		// 	justifyContent: 'center',
-		// }}
-		>
+		<div>
 			<Row style={{ display: 'flex', justifyContent: 'center' }}>
 				<h1>Detalhes do pedido</h1>
 			</Row>
 			<Row style={{ display: 'flex', justifyContent: 'space-around' }}>
 				<Col>
-					<h2>Entrega</h2>
+					<List
+						bordered
+						size="small"
+						header={<h2>Entrega</h2>}
+						dataSource={shipping}
+						renderItem={(item) => <List.Item>{item}</List.Item>}
+					/>
 				</Col>
 				<Col>
-					<h2>Método de pagamento</h2>
+					<List
+						bordered
+						size="small"
+						header={<h2>Método de pagamento</h2>}
+						dataSource={paymentMethod}
+						renderItem={(item) => <List.Item>{item}</List.Item>}
+					/>
 				</Col>
 				<Col>
-					<h2>Resumo do pedido</h2>
+					<List
+						bordered
+						size="small"
+						header={<h2>Resumo do pedido</h2>}
+						dataSource={orderSummary}
+						renderItem={(item) => <List.Item>{item}</List.Item>}
+					/>
+				</Col>
+			</Row>
+			<Row
+				style={{
+					display: 'flex',
+					flexDirection: 'column',
+				}}
+			>
+				<h2>Itens do pedido</h2>
+				<Col>
+					<List>
+						{Array.from({ length: 3 }).map((item, idx) => (
+							<CartItemComponent key={idx} />
+						))}
+					</List>
 				</Col>
 			</Row>
 		</div>
