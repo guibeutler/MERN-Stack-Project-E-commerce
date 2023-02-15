ef1f00ac3bcb4bd3cf49826408f7bd3ca160664b
Guilherme <guilherme.beutler@outlook.com>
2023-02-14 22:59:51 -0300

    prepare user order details

--- frontend/src/screens/users/UserOrderDetails.tsx
+++ frontend/src/screens/users/UserOrderDetails.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Col, Layout, Row } from 'antd'
 
 function UserOrderDetails() {
 	return (
-		<div>
-			<p>USER ORDER DETAILS</p>
+		<div
+		// style={{
+		// 	display: 'flex',
+		// 	flexDirection: 'column',
+		// 	justifyContent: 'center',
+		// }}
+		>
+			<Row style={{ display: 'flex', justifyContent: 'center' }}>
+				<h1>Detalhes do pedido</h1>
+			</Row>
+			<Row style={{ display: 'flex', justifyContent: 'space-around' }}>
+				<Col>
+					<h2>Entrega</h2>
+				</Col>
+				<Col>
+					<h2>Método de pagamento</h2>
+				</Col>
+				<Col>
+					<h2>Resumo do pedido</h2>
+				</Col>
+			</Row>
 		</div>
 	)
 }
