c3b8e5bcc41b45e266670a8d90c1ce8674e2c21c
Guilherme <guilherme.beutler@outlook.com>
2023-03-27 22:36:09 -0300

    adjustment admin navbar

--- frontend/src/components/AdminNavbar/index.tsx
+++ frontend/src/components/AdminNavbar/index.tsx
@@ -94,12 +94,18 @@
 import React, { useState } from 'react'
 import {
 	AppstoreOutlined,
+	ArrowLeftOutlined,
 	ContainerOutlined,
 	DesktopOutlined,
+	InboxOutlined,
+	LineChartOutlined,
 	MailOutlined,
 	MenuFoldOutlined,
 	MenuUnfoldOutlined,
+	MessageOutlined,
 	PieChartOutlined,
+	UnorderedListOutlined,
+	UserOutlined,
 } from '@ant-design/icons'
 import type { MenuProps } from 'antd'
 import { Button, Menu } from 'antd'
@@ -123,26 +129,12 @@ function getItem(
 }
 
 const items: MenuItem[] = [
-	getItem('Option 1', '1', <PieChartOutlined />),
-	getItem('Option 2', '2', <DesktopOutlined />),
-	getItem('Option 3', '3', <ContainerOutlined />),
-
-	getItem('Navigation One', 'sub1', <MailOutlined />, [
-		getItem('Option 5', '5'),
-		getItem('Option 6', '6'),
-		getItem('Option 7', '7'),
-		getItem('Option 8', '8'),
-	]),
-
-	getItem('Navigation Two', 'sub2', <AppstoreOutlined />, [
-		getItem('Option 9', '9'),
-		getItem('Option 10', '10'),
-
-		getItem('Submenu', 'sub3', null, [
-			getItem('Option 11', '11'),
-			getItem('Option 12', '12'),
-		]),
-	]),
+	getItem('Pedidos', '1', <UnorderedListOutlined />),
+	getItem('Produtos', '2', <InboxOutlined />),
+	getItem('Usuarios', '3', <UserOutlined />),
+	getItem('Mensagens', '4', <MessageOutlined />),
+	getItem('Estatisticas', '5', <LineChartOutlined />),
+	getItem('Sair', '6', <ArrowLeftOutlined />),
 ]
 
 const AdminNavbar: React.FC = () => {
